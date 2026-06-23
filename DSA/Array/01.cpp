ac66c0718c0aa118fe7264f2c3eabdf797c565fb
Suhani Tomar <suhanitomar888@gmail.com>
2026-06-24 00:52:18 +0530

    dsa today

---
+++ DSA/Array/01.cpp
@@ -0,0 +1,13 @@
+// https://www.geeksforgeeks.org/problems/check-if-an-array-is-sorted0701/1
+// class Solution {
+//   public:
+//     bool isSorted(vector<int>& arr) {
+//         // code here
+//         for(int i=0;i<arr.size()-1;i++){
+//             if(arr[i]>arr[i+1]){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
