eb1e8d97add23548442af351e7c7dd3f99c45896
Suhani Tomar <suhanitomar888@gmail.com>
2026-06-24 01:02:22 +0530

    ..

---
+++ CPP/functions.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+//int main(){
+        //functions
+// void func1(int num){
+//     cout<<"Value of num is: "<<num<<endl;
+// }
+// int func2(int num){
+//     int ans=2*num;
+//     return ans;
+// }
+// int main(){
+//     func1(10);
+//     cout<<func2(10)<<endl;
+// }
+
+// void doubleTheNumber(int num){
+//     num*=2;
+//     cout<<"inside the function :"<<num<<endl;
+// }
+// //if & then original value of num will be changed,otherwise a copy of num will be created and the original value will remain unchanged
+// int main(){
+//     int num=10;
+//     doubleTheNumber(num);
+//     cout<<"outside the function :"<<num<<endl;
+// }
+//}
+
+
