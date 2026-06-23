e8f707754e2a818a63d763530ec2e8f80d638222
Suhani Tomar <suhanitomar888@gmail.com>
2026-06-24 01:04:01 +0530

    ...

---
+++ CPP/classesNdobjects.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next;
+    //constructor method- same name as class name, no return type, can have parameters
+    Node(int val){
+        data=val;
+        next=NULL;
+    }{
+}
+
+int main(){
+   
+} 
+    noe
