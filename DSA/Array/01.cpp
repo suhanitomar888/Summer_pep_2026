235b6995b9fa107c4743a30881ff1d6e72121006
Suhani Tomar <suhanitomar888@gmail.com>
2026-06-24 03:04:42 +0530

    day1 dsa

--- DSA/Array/01.cpp
+++ DSA/Array/01.cpp
@@ -11,3 +11,15 @@
 //         return true;
 //     }
 // };
+
+// https://leetcode.com/problems/running-sum-of-1d-array/submissions/2043890906/
+// class Solution {
+// public:
+//     vector<int> runningSum(vector<int>& nums){
+//         for(int i=1;i<nums.size();i++){
+//             nums[i]+=nums[i-1];
+//         }
+//         return nums;
+//     }
+// };
+
