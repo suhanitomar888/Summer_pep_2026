acf1e2822982b0442b27473b1bf93343cf5e4efb
Suhani Tomar <suhanitomar888@gmail.com>
2026-06-26 11:57:21 +0530

    ...

---
+++ DSA/Array/04.cpp
@@ -0,0 +1,7 @@
+// https://www.geeksforgeeks.org/problems/pair-sum-in-a-sorted-and-rotated-array/1
+
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+
+// https://www.geeksforgeeks.org/problems/floor-in-a-sorted-array-1587115620/1
+
+// 
