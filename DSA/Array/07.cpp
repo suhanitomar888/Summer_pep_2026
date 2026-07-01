25882e957cbb5c86f0301cfe9f70c3ca8e73ade6
Suhani Tomar <suhanitomar888@gmail.com>
2026-07-01 09:29:09 +0530

    ..

--- DSA/Array/07.cpp
+++ DSA/Array/07.cpp
@@ -1,70 +1,74 @@
 //head nd tail recursion
 
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-void print(int n){
-    if(n==10){
-        return;
-    }
-    cout << n << " ";
-    print(n+1);
-}
-void printReverse(int n){
-    if(n==0){
-        return;
-    }
-    cout << n << " ";
-    printReverse(n-1);
-}
-int main(){
-    print(1);
-    return 0;
-}
+// void print(int n){
+//     if(n==10){
+//         return;
+//     }
+//     cout << n << " ";
+//     print(n+1);
+// }
+// void printReverse(int n){
+//     if(n==0){
+//         return;
+//     }
+//     cout << n << " ";
+//     printReverse(n-1);
+// }
+// int main(){
+//     print(1);
+//     return 0;
+// }
 
-// factorial uisng recursion
-int factorial(int n){if(n==0){
-     if(n==0 ||n==1)return 1;
-    return n * factorial(n-1);
-}
+// // factorial uisng recursion
+// int factorial(int n){if(n==0){
+//      if(n==0 ||n==1)return 1;
+//     return n * factorial(n-1);
+// }
 
-// fibonacci using recursion
-//Recusion
+// // fibonacci using recursion
+// //Recusion
 
-bool binarySearch(int arr[], int l, int r, int key){
-    //base case->
-    //int mid
-    //if(arr[mid]==key)return true;
-    //else if(arr[mid]<key){
-    //}
-    //else{
-    //}
+// bool binarySearch(int arr[], int l, int r, int key){
+//     //base case->
+//     //int mid
+//     //if(arr[mid]==key)return true;
+//     //else if(arr[mid]<key){
+//     //}
+//     //else{
+//     //}
 
-    if(l>r){
-        return false;
-    }
-    int mid=l+(r-l)/2;
-    if(arr[mid]==key){
-        return true;
-    }else if(arr[mid]>key){
-        return binarySearch(arr,l,mid-1,key);
-    }else{
-        return binarySearch(arr,mid+1,r,key);
-    }
-}
-//check if array is sorted or not using recursion
-bool isSorted(int arr[], int n){
-    if(n==1 || n==0){
-        return true;
-    }
-    if(arr[n-1]<arr[n-2]){
-        return false;
-    }
-    return isSorted(arr,n-1);
-}
+//     if(l>r){
+//         return false;
+//     }
+//     int mid=l+(r-l)/2;
+//     if(arr[mid]==key){
+//         return true;
+//     }else if(arr[mid]>key){
+//         return binarySearch(arr,l,mid-1,key);
+//     }else{
+//         return binarySearch(arr,mid+1,r,key);
+//     }
+// }
+// //check if array is sorted or not using recursion
+// bool isSorted(int arr[], int n){
+//     if(n==1 || n==0){
+//         return true;
+//     }
+//     if(arr[n-1]<arr[n-2]){
+//         return false;
+//     }
+//     return isSorted(arr,n-1);
+// }
 
 // 2D- Array
 //traversal
+
+#include<iostream>
+using namespace std;
+
 int[3][4]
 for(int i=0;i<3;i++){
     for(int j=0;j<4;j++){
@@ -105,3 +109,12 @@ void printOddColumn(int arr[4][4],int row,int col){
         }
     }       
 }
+//create a method, pass 2d array as parameter, nd show wht is happening
+void aaa(int arr[3][4],int row,int col){
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            cout<<arr[i][j]<<" ";
+        }
+    }
+}
+//
