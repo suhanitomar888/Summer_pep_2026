72af8fc4ae159c4fe4b5e6e6e7460a75ce5308f0
Suhani Tomar <suhanitomar888@gmail.com>
2026-07-02 09:41:03 +0530

    ...

--- DSA/Strings/01.cpp
+++ DSA/Strings/01.cpp
@@ -1,7 +1,30 @@
 #include<iostream>
+#include<string>
+
 using namespace std;
+
+//using pointers
+
+string reverseString(string str){
+    // int a= str.begin();
+    // int b= str.end()-1;
+    // while(a<b){
+    //     swap(*a,*b);
+    //     a++;
+    //     b--;
+    // }
+    // return str;
+
+    int l=0, r=s.length()-1;
+    while(l<r){
+        swap(s[l++],s[r++]);
+        l++;
+        r--;
+    }
+}
 int main(){
-    string name;
-    name="Hello";
-    cout<<name<<endl;
+    string str="Hello";
+    cout<<reverseString(str)
+];
+
 }
---
+++ DSA/Strings/02.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/valid-anagram/
+
+// https://leetcode.com/problems/valid-palindrome/description/
+ 
+
+
+
+
+
+
+
+
+
+
+
