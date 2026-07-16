c6de3c34dd587111abae5050a3cf6c85dcfd022b
Suhani Tomar <suhanitomar888@gmail.com>
2026-07-16 11:55:59 +0530

    Add Node class and implement level order traversal function

---
+++ DSA/Trees/03.cpp
@@ -0,0 +1,30 @@
+class Node{
+public:
+    int data;
+    Node*left;
+    Node*right;
+    Node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+//Level Order Traversal
+void leverOrder(Node*root){
+    if(root==NULL)return;
+
+    //first create a queue and push root and NULL;
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()){
+        Node* frontNode =q.front();
+        cout<< frontNode->data <<" ";
+        if(frontNode->left) q.push(frontNode->left);
+        if(frontNode->right) q.push(front->right);
+    }
+    else if(!q.empty()){
+        q.push(NULL);
+    }
+}
