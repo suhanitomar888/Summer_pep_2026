2858a3d96374079498d541492d0359c03163e48b
Suhani Tomar <suhanitomar888@gmail.com>
2026-07-22 18:22:59 +0530

    Add DFS implementation for counting provinces in 01.cpp

--- DSA/Graphs/01.cpp
+++ DSA/Graphs/01.cpp
@@ -70,3 +70,36 @@ class Solution {
     }
 };
 */
+
+// https://leetcode.com/problems/number-of-provinces/
+/*
+class Solution {
+public:
+
+    void dfsHelper(int city, vector<vector<int>>& isConnected, vector<bool>& isVisited) {
+        isVisited[node] = true;
+
+        for (int neighbour = 0; neighbour < isConnected.size(); neighbour++) {
+            if (isConnected[node][neighbour] == 1 && !visited[neighbour]) {
+                dfsHelper(neighbour, isConnected, visited);
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n= isConnected.size();
+
+        vector<bool> isVisited(n, false);
+        int provinceCount = 0;
+
+        for(int i=0;i<n;i++){
+            if(!isVisited[i]){
+                //dfsHelper
+                provinceCount++;
+            }
+        }
+        return provinceCount;
+    }
+};
+*/
+
+//
