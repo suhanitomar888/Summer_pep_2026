9cc39c626abdf696f28eb2dd7c68615b8ea5a82a
Suhani Tomar <suhanitomar888@gmail.com>
2026-07-24 10:35:29 +0530

    Add commented implementation of topological sort algorithm in 02.cpp

--- DSA/Graphs/02.cpp
+++ DSA/Graphs/02.cpp
@@ -24,3 +24,42 @@ public:
 };*/
 
 // https://www.geeksforgeeks.org/problems/topological-sort/1
+/*
+class Solution {
+  public:
+    void dfs (int node, vector<vector<int>> &adj, vector<bool> &visited, stack<int> &st) {
+        visited[node] = true;
+        
+        for(auto neighbour : adj[node]) {
+            if(!visited[neighbour]) {
+                dfs(neighbour, adj, visited, st);
+            }
+        }
+        st.push(node);
+    }
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        // code here
+        vector<vector<int>> adj(V);
+        for(auto i : edges ) {
+            int u = i[0];
+            int v = i[1];
+            
+            adj[u].push_back(v);
+        }
+        
+        vector<bool> visited(V, false);
+        stack<int> st;
+        for(int i=0; i<V; i++) {
+            if(!visited[i]) {
+                dfs(i, adj, visited, st);
+            }
+        }
+        vector<int> res;
+        while(!st.empty()) {
+            res.push_back(st.top());
+            st.pop();
+        }
+        return res;
+    }
+};
+*/
